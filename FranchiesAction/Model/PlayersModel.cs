01e898c5e8104dea074f06278c295b5f04749881
jiyavudeen.mohamed <jiyavudeen.mohamed@infosys.com>
2019-04-29 19:10:59 +0530

    Add project files.

---
+++ FranchiesAction/Model/PlayersModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FranchiesAction.Model
+{
+   public class PlayersModel 
+    {
+        private int randomNumber;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public int RandomNumber
+        {
+            get
+            {
+                return randomNumber;
+            }
+            set
+            {
+                randomNumber = value;
+                OnPropertyChanged("RandomNumber");
+            }
+        }
+    }
+}
---
+++ FranchiesAction/ViewModel/PlayerViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FranchiesAction.Model;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace FranchiesAction.ViewModel
+{
+    //connectionString="Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=fantacyLeague_db;Integrated Security=SSPI;"
+    public class PlayerViewModel : BindableBase
+    {
+        private ICommand randomNumberCommand { get; set; }
+
+        public PlayerViewModel()
+        {
+
+        }
+
+        public int X
+        {
+            get { return _x; }
+            set
+            {
+                SetProperty(ref _x, value);
+                OnPropertyChanged(() => Z);
+            }
+        }
+        public ICommand RandomNumberCommand
+        {
+            get
+            {
+                return randomNumberCommand;
+            }
+            set
+            {
+                randomNumberCommand = value;
+                OnPropertyChanged("RandomNumber");
+            }
+        }
+
+    }
+
+
+    
+}
+   
